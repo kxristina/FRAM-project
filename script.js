a1350eb4aa5a6ca94dc0f445ebf2b4b435a0a0d8
Kristina <kristina.kehagia@hotmail.comgit config --global --listgit config --global user.name Kristinagit config --global user.email kristina.kehagia@hotmail.com>
2026-06-05 07:28:25 +0200

    Added side menu, hover states and navigation to buttons

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const sideMenu = document.getElementById("sideMenu");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+});
+
+
+
+const productsLink = document.getElementById("products-link");
+
+productsLink.addEventListener("click", function(event) {
+
+    event.preventDefault();
+
+    window.location.href = "products.html";
+
+});
+
+
+
+
+const productsBtn = document.getElementById("products-btn");
+
+productsBtn.addEventListener("click", function() {
+    window.location.href = "products.html";
+});
+
+
+
 function initMap() {
 
     const farmLocation = {
